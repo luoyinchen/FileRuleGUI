8a7cada64094212a284aa9858d4a34faa4c9286d
luoyinchen <luoyinchen@omcube.com>
2023-02-02 16:31:59 +0800

    新增 pom 依赖以及打包插件
    主程序入口

---
+++ src/main/java/org/moretia/filerule/Application.java
@@ -0,0 +1,14 @@
+package org.moretia.filerule;
+
+/**
+ * @author moretia
+ * @version 1.0
+ * @className Application
+ * @description 主程序
+ * @date 2023/2/2
+ **/
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
